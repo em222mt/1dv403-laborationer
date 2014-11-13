91c1a67c19cfb4ab0bcd618809126a6e8ff584cc
Erik Myrsell <em222mt@student.lnu.se>
2014-11-13 09:33:14 +0000

    Börjat med uppgift 1.4

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,21 @@
 
 var makePerson = function(persArr){
 
-
-	// Din kod här...
-
-}
-
+    var i = 0;
+    var averageAge = 0;
+    var ages = [];
+    var names = [];
+	
+    for(i = 0; i < persArr.length; i+=1){
+        names[i] = persArr[i].name;
+        ages[i] = persArr[i].age;
+        averageAge += persArr[i].age;
+    }
+   
+};    
+	
+    var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+ 
+    var result = makePerson(data);
+ 
+    console.log(result);
