f0052095060bf2a8e03f8d49917d3b84189483d9
Erik Myrsell <em222mt@student.lnu.se>
2014-11-15 12:09:50 +0000

    lagt tiil enkel CSS och HTML

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -4,10 +4,27 @@ var MessageBoard = {
     
     messages: [],
     
-    init:function(e)
-    {
+    init: function(e){
+        // var mess = new Message();
+        var text = document.getElementById("formtext");
         
-        var mess = new Message();
-    }
-}
+        var submit = document.getElementById("send");
+    },
     
+    // renderMessages: function(){
+    //     //Ta bort alla meddelanden
+    //     document.getElementById("formtext".innerHTML = "");
+        
+    //     //Rendera alla meddelanden
+    //     for (var i = 0; i < MessageBoard.messages.length; ++i){
+    //         MessageBoard.renderMessage(i);
+    //     }
+    // },
+    
+    // renderMessage: function(text){
+    //     //Kod här för att ta bort ett meddelande.
+    // }
+    
+};
+
+window.onload = MessageBoard.init;
